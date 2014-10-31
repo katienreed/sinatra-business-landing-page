6a6e54899a25e078dca7d436bd7865399ae54598
katie.reed@me.com <katie.reed@me.com>
2014-10-31 12:08:17 -0400

    Added social media buttons and modified the mandarill app without completion

--- app.rb
+++ app.rb
@@ -14,5 +14,21 @@ get '/contact' do
 end
 
 post '/post' do
+	m = Mandrill::API.new
+	message = {
+		:subject => "Greetings from Rocket Fuel!",
+		:from_name => "Rocket Fuel Coffee Co.",
+		:text => "Thanks for visiting our website!  We will be in contact with you shortly. :)",
+		:to => [
+			{
+				:email => "recipient@theirdomain.com",
+				:name => "Recipient1"
+			}
+		],
+		:html => "<html><h1>Hello, thanks for visitng Rocket Fuel Coffee!</h1></html>",
+		:from_email => "sender@yourdomain.com"
+	}
+	sending = m.messages.send message
+	puts sending
 	erb :post
 end
